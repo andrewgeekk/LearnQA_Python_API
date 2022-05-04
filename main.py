cfa6171c8474343e14081651623747b698967ee5
andrewgeekk <andrewgeekk@yandex.net>
2022-05-04 12:43:15 +0300

    Ex7: Запросы и методы

--- main.py
+++ main.py
@@ -1,8 +1,26 @@
 import requests
 
-response = requests.get("https://playground.learnqa.ru/api/long_redirect")
-response_amount = response.history[-1]
-print(f'Итоговый URL: {response_amount.url}')
-print(f'Количество редиректов: {len(response.history)}')
-for i in range(len(response.history)):
-    print(f'{i+1}) {response.history[i].url}')
+URL = 'https://playground.learnqa.ru/ajax/api/compare_query_type'
+method = {"method":"GET"}
+response1 = requests.get(URL)
+print(f'1. Без параметра method: {response1.text}')
+
+response2 = requests.head(URL, data='HEAD')
+print(f'2. Не из списка HEAD: {response2.text}')
+
+response3 = requests.post(URL, data={"method":"POST"})
+print(f'3. с правильным значением method: {response3.text}')
+
+print('4.')
+methods = ['POST', 'GET', 'PUT', 'DELETE', 'HEAD']
+type = [[requests.get, 'requests.get'], [requests.post, 'requests.post'], [requests.put, 'requests.put'],
+        [requests.delete, 'requests.delete'], [requests.head, 'requests.head']]
+
+for i in type:
+    for j in methods:
+        if i[1] == 'requests.get':
+            response = i[0](URL, params={"method":j})
+            print(f'{i[1]}({URL}, parasms=["method"="{j}"]):  {response.text}')
+        else:
+            response = i[0](URL, data={"method": j})
+            print(f'{i[1]}({URL}, data=["method"="{j}"]):  {response.text}')
