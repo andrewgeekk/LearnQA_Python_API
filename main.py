ae2326d3f1d94a362912fc8b8347fe0ef570b7c9
andrewgeekk <andrewgeekk@yandex.net>
2022-05-05 11:18:50 +0300

    Ex6: Длинный редирект

--- main.py
+++ main.py
@@ -1,12 +1,1 @@
-import requests
 
-response = requests.get("https://playground.learnqa.ru/api/long_redirect", allow_redirects=True)
-response_amount = len(response.history)
-first_response = response.history[0].url
-second_response = response.history[1].url
-finally_response = response.url
-
-print(f'Итоговый URL: {response.url}')
-print(f'Количество редиректов: {response_amount}')
-
-print(first_response, second_response, finally_response, sep='\n')
